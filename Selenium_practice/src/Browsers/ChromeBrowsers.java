cf17dfdd775251d7bda82362078a2d63caf388ad
sureshnemani <nemani.sureshbabu@gmail.com>
2018-07-08 17:55:38 +0530

    1st pro

---
+++ Selenium_practice/src/Browsers/ChromeBrowsers.java
@@ -0,0 +1,17 @@
+package Browsers;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class ChromeBrowsers {
+
+	public static void main(String[] args) {
+		
+		System.setProperty("webriver.chrome.driver", "D:\\Selenium Stuff\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("http://google.com");
+		
+
+	}
+
+}
