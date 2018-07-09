9afd0a5e5045a53593bad4e3f9e17663dcd5225e
sureshnemani <nemani.sureshbabu@gmail.com>
2018-07-09 10:21:31 +0530

    get url

--- Selenium_practice/src/Browsers/ChromeBrowsers.java
+++ Selenium_practice/src/Browsers/ChromeBrowsers.java
@@ -7,9 +7,11 @@ public class ChromeBrowsers {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webriver.chrome.driver", "D:\\Selenium Stuff\\chromedriver_win32\\chromedriver.exe");
+		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Stuff\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("http://google.com");
+		System.out.println(driver.getTitle());
+		System.out.println(driver.getCurrentUrl());
 		
 
 	}
