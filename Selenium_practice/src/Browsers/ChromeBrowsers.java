3663e8c68eea271f8f1e8d056064c3aebc11927b
sureshnemani <nemani.sureshbabu@gmail.com>
2018-07-09 10:26:12 +0530

    modify

--- Selenium_practice/src/Browsers/ChromeBrowsers.java
+++ Selenium_practice/src/Browsers/ChromeBrowsers.java
@@ -10,7 +10,7 @@ public class ChromeBrowsers {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Stuff\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver= new ChromeDriver();
 		driver.get("http://google.com");
-
+		
 		System.out.println(driver.getTitle());
 		System.out.println(driver.getCurrentUrl());
 		
