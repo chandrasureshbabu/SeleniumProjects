b7aa01a659d4aecfc3e8c54f9fa872fda04e1b26
sureshnemani <nemani.sureshbabu@gmail.com>
2018-08-26 16:20:18 +0530

    css modified

--- Selenium_practice/src/Locators/Css_Selectors.java
+++ Selenium_practice/src/Locators/Css_Selectors.java
@@ -12,9 +12,10 @@ public class Css_Selectors {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium Stuff\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://login.salesforce.com/?locale=in");
-		driver.findElement(By.cssSelector("#username")).sendKeys("Username");
-		driver.findElement(By.cssSelector("[id='password']")).sendKeys("Password");
-		driver.findElement(By.cssSelector("#Login")).click();
+		driver.findElement(By.cssSelector("#username")).sendKeys("Username");// if id is there we can write #idvalue
+		driver.findElement(By.cssSelector("[id='password']")).sendKeys("Password");// here we didnt use tagname in css we leave tagname
+		driver.findElement(By.cssSelector(".button.r4.wide.primary")).click();//if class value is there we can write css as .class vlaue ,ex- ".button.r4.wide.primary"
+		//driver.findElement(By.cssSelector("#Login")).click();
 		driver.quit();
 
 	}
